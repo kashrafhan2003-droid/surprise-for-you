2a44642664cfcd464288312b022ae37bf3319d9e
kashrafhan2003-droid <kashrafhan2003@gmail.com>
2026-07-07 13:34:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+const proposal = document.getElementById("proposal");
+const mainCard = document.getElementById("mainCard");
+
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
 const btn = document.getElementById("playBtn");
 const song = document.getElementById("song");
 const surprise = document.getElementById("surprise");
 const card = document.querySelector(".card");
 
-btn.addEventListener("click", () => {
+// No button bhaagega
+function moveNoButton() {
+    const x = Math.random() * 250 - 125;
+    const y = Math.random() * 150 - 75;
+
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+noBtn.addEventListener("mouseover", moveNoButton);
+
+noBtn.addEventListener("touchstart", function(e){
+    e.preventDefault();
+    moveNoButton();
+});
+
+// YES CLICK
+yesBtn.addEventListener("click", () => {
+
+    proposal.style.display = "none";
+    mainCard.style.display = "block";
 
     song.play();
 
@@ -17,10 +42,11 @@ btn.addEventListener("click", () => {
 
     setTimeout(() => {
         surprise.classList.add("show");
-    }, 5000);
+    },5000);
 
 });
 
+// Sparkles
 function createSparkles(){
 
     for(let i=0;i<25;i++){
@@ -53,6 +79,7 @@ function createSparkles(){
 
 }
 
+// Hearts
 function createHeart(){
 
     const heart=document.createElement("div");
@@ -62,13 +89,9 @@ function createHeart(){
     heart.style.position="fixed";
     heart.style.left=Math.random()*100+"vw";
     heart.style.top="100vh";
-
     heart.style.fontSize=(15+Math.random()*30)+"px";
-
     heart.style.opacity=Math.random();
-
     heart.style.pointerEvents="none";
-
     heart.style.zIndex="1";
 
     document.body.appendChild(heart);
@@ -76,17 +99,11 @@ function createHeart(){
     let duration=5000+Math.random()*5000;
 
     heart.animate([
-
-        {transform:"translateY(0) rotate(0deg)",opacity:1},
-
-        {transform:"translateY(-120vh) rotate(360deg)",opacity:0}
-
+        {transform:"translateY(0)",opacity:1},
+        {transform:"translateY(-120vh)",opacity:0}
     ],{
-
         duration:duration,
-
         easing:"linear"
-
     });
 
     setTimeout(()=>{
@@ -97,14 +114,15 @@ function createHeart(){
 
 setInterval(createHeart,300);
 
+// Loader
 window.addEventListener("load",()=>{
 
-setTimeout(()=>{
+    setTimeout(()=>{
 
-document
-.getElementById("loader")
-.classList.add("loader-hide");
+        document
+        .getElementById("loader")
+        .classList.add("loader-hide");
 
-},2500);
+    },2500);
 
 });
